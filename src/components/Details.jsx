ec25949b91963787d695b505fc8b4af90718c355
said-ops <idrissisaid29@gmail.com>
2024-10-15 04:07:53 +0100

    details page dynamic

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import useHomeStore from "../store/homeStore";
-import { useParams } from "react-router-dom";
+import { Link, useParams,useNavigate } from "react-router-dom";
 
 function Details() {
   const theme = useHomeStore((state) => state.theme);
@@ -9,15 +9,16 @@ function Details() {
   const details = useHomeStore((state) => state.details);
   const detailsLoading = useHomeStore((state) => state.detailsLoading);
   const detailsError = useHomeStore((state) => state.detailsError);
-  const {id} = useParams();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-    fetchDetails(id)
-  },[id])
+  useEffect(() => {
+    fetchDetails(id);
+  }, [id]);
   return (
     <>
       <Navbar />
-      <button className="flex items-center gap-4 w-[8rem] h-10 p-4 md:ml-16 ml-4 hover:shadow-xl rounded-md shadow-md bg-element-light text-text-light dark:bg-element-dark dark:text-text-dark">
+      <button onClick={()=>navigate(-1)} className="flex items-center gap-4 w-[8rem] h-10 p-4 md:ml-16 ml-4 hover:shadow-xl rounded-md shadow-md bg-element-light text-text-light dark:bg-element-dark dark:text-text-dark">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="30px"
@@ -44,88 +45,96 @@ function Details() {
       </button>
 
       {/* details goes here */}
-      {details&&!detailsLoading&&!detailsError&&(
+      {details && !detailsLoading && !detailsError && (
         <div className="md:px-16 p-4 pb-8 flex gap-8 flex-col md:flex-row md:gap-32 text-center md:text-left">
-        {/* flag */}
-        <div className="self-center ">
-          <img
-            className="object-fit md:w-[24.5rem] md:h-[18.5rem]"
-            src={`https://flagcdn.com/w640/${details.cca2.toLowerCase()}.png`}
-            alt="flag of"
-          />
-        </div>
-        {/* stats */}
-        <div className="flex flex-col gap-8">
-          <h2 className="font-extrabold text-3xl ">Belgium</h2>
-          <div className="flex flex-col md:flex-row gap-16">
-            <div className="flex flex-col gap-2">
-              <p className="text-gray-600 dark:text-gray-200">
-                <span className="font-semibold  text-md text-text-light dark:text-text-dark">
-                  Native Name
-                </span>
-                {/* : {country.population} */} : 100000
-              </p>
-              <p className="text-gray-600 dark:text-gray-200">
-                <span className="font-semibold  text-md text-text-light dark:text-text-dark">
-                  Population
-                </span>
-                {/* : {country.population} */} : 100000
-              </p>
-              <p className="text-gray-600 dark:text-gray-200">
-                <span className="font-semibold  text-md text-text-light dark:text-text-dark">
-                  Region
-                </span>
-                {/* : {country.region} */} : Europe
-              </p>
-              <p className="text-gray-600 dark:text-gray-200">
-                <span className="font-semibold  text-md text-text-light dark:text-text-dark">
-                  Sub Region
-                </span>
-                {/* : {country.capital} */} : Berlin
-              </p>
-              <p className="text-gray-600 dark:text-gray-200">
-                <span className="font-semibold  text-md text-text-light dark:text-text-dark">
-                  Capital
-                </span>
-                {/* : {country.capital} */} : Berlin
-              </p>
-            </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-gray-600 dark:text-gray-200">
-                <span className="font-semibold  text-md text-text-light dark:text-text-dark">
-                  Top Level Domain
-                </span>
-                {/* : {country.population} */} : 100000
-              </p>
-              <p className="text-gray-600 dark:text-gray-200">
-                <span className="font-semibold  text-md text-text-light dark:text-text-dark">
-                  Currencies
-                </span>
-                {/* : {country.population} */} : 100000
-              </p>
-              <p className="text-gray-600 dark:text-gray-200">
-                <span className="font-semibold  text-md text-text-light dark:text-text-dark">
-                  Languages
-                </span>
-                {/* : {country.region} */} : Europe
-              </p>
-            </div>
+          {/* flag */}
+          <div className="self-center ">
+            <img
+              className="object-fit md:w-[24.5rem] md:h-[18.5rem]"
+              src={`https://flagcdn.com/w640/${details.cca2.toLowerCase()}.png`}
+              alt="flag of"
+            />
           </div>
-          {/* border countries */}
-          <div className="flex items-center flex-wrap gap-4">
-            <span>Border Countries : </span>
-            <span className="bg-element-light text-text-light p-2 rounded-sm shadow-lg dark:bg-element-dark dark:text-text-dark">
-                France
-            </span>
-            <span className="bg-element-light text-text-light p-2 rounded-sm shadow-lg dark:bg-element-dark dark:text-text-dark">
-                France
-            </span>
-            <span className="bg-element-light text-text-light p-2 rounded-sm shadow-lg dark:bg-element-dark dark:text-text-dark">
-                France
-            </span>
+          {/* stats */}
+          <div className="flex flex-col gap-8">
+            <h2 className="font-extrabold text-3xl ">{details.name.common}</h2>
+            <div className="flex flex-col md:flex-row gap-16">
+              <div className="flex flex-col gap-2">
+                <p className="text-gray-600 dark:text-gray-200">
+                  <span className="font-semibold  text-md text-text-light dark:text-text-dark">
+                    Native Name
+                  </span>
+                  {/* : {country.population} */} : {details.name.official}
+                </p>
+                <p className="text-gray-600 dark:text-gray-200">
+                  <span className="font-semibold  text-md text-text-light dark:text-text-dark">
+                    Population
+                  </span>
+                  {/* : {country.population} */} : {details.population}
+                </p>
+                <p className="text-gray-600 dark:text-gray-200">
+                  <span className="font-semibold  text-md text-text-light dark:text-text-dark">
+                    Region
+                  </span>
+                  {/* : {country.region} */} : {details.region}
+                </p>
+                <p className="text-gray-600 dark:text-gray-200">
+                  <span className="font-semibold  text-md text-text-light dark:text-text-dark">
+                    Sub Region
+                  </span>
+                  {/* : {country.capital} */} : {details.subregion}
+                </p>
+                <p className="text-gray-600 dark:text-gray-200">
+                  <span className="font-semibold  text-md text-text-light dark:text-text-dark">
+                    Capital
+                  </span>
+                  {/* : {country.capital} */} : {details.capital}
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-gray-600 dark:text-gray-200">
+                  <span className="font-semibold  text-md text-text-light dark:text-text-dark">
+                    Top Level Domain
+                  </span>
+                  {/* : {country.population} */} : {details.tld}
+                </p>
+                <p className="text-gray-600 dark:text-gray-200">
+                  <span className="font-semibold  text-md text-text-light dark:text-text-dark">
+                    Currencies
+                  </span>
+                  {/* : {country.population} */} :{" "}
+                  {Object.keys(details.currencies).map((key, index) => {
+                    return <span key={index}>{key}</span>;
+                  })}
+                </p>
+                <p className="text-gray-600 dark:text-gray-200">
+                  <span className="font-semibold  text-md text-text-light dark:text-text-dark">
+                    Languages
+                  </span>
+                  {/* : {country.region} */} :{" "}
+                  {Object.keys(details.languages).map((key, index) => {
+                    return <span key={index}>{key}</span>;
+                  })}
+                </p>
+              </div>
+            </div>
+            {/* border countries */}
+            {details.borders && (
+              <div className="flex items-center flex-wrap gap-4">
+                <span>Border Countries : </span>
+                {details.borders.map((key, index) => {
+                  return (
+                   <Link key={index} to={`/details/${key}`}>
+                     <span  className="bg-element-light text-text-light p-2 rounded-sm shadow-lg dark:bg-element-dark dark:text-text-dark">
+                      {key}
+                    </span>
+                   </Link>
+                  );
+                })}
+              </div>
+            )}
           </div>
         </div>
-      </div>
       )}
     </>
   );
